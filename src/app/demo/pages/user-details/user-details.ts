49aef38414a329fd2de7b71c80829f43a4f52d4d
mina1234emad-admin <mina.emadnaguib@vodafone.com>
2025-11-16 22:37:16 +0200

    Create the design of user details component

--- src/app/demo/pages/user-details/user-details.ts
+++ src/app/demo/pages/user-details/user-details.ts
@@ -11,8 +11,16 @@ import { SharedModule } from 'src/app/theme/shared/shared.module';
   styleUrl: './user-details.scss'
 })
 export class UserDetails implements OnInit {
+  previewImage: string = 'assets/images/user/avatar-4.jpg';
   userId: string | null = null;
   user: any;
+
+  form = {
+    fullName: '',
+    mobile: '',
+    grade: '',
+    birthDate: ''
+  };
 
   constructor(private router: Router, private userService: UserService, private route: ActivatedRoute) { }
 
@@ -26,9 +34,41 @@ export class UserDetails implements OnInit {
     if (!this.user) {
       // 1- navigate to users component again
       // this.router.navigate(['/users']);
-      
+
       // 2- get the data from database
       // console.log('No user in state — fetch from API by ID:', userId);
     }
+  }
+
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewImage = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  saveChanges() {
+    const body = {
+      fullName: this.form.fullName,
+      mobile: this.form.mobile,
+      grade: this.form.grade,
+      birthDate: this.form.birthDate
+    };
+
+    console.log("body: " + body.fullName);
+    console.log("body: " + body.birthDate);
+    console.log("body: " + body.grade);
+    console.log("body: " + body.mobile);
+
+    // this.http.post('https://your-api-url.com/api/update-user', body)
+    //   .subscribe({
+    //     next: res => console.log("Saved successfully", res),
+    //     error: err => console.error("Error", err)
+    //   });
   }
 }
