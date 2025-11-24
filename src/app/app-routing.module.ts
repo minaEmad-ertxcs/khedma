3a648a4897565d4dff27b107725c0d81e3ddfbe5
mina1234emad-admin <mina.emadnaguib@vodafone.com>
2025-11-24 11:51:12 +0200

    Build and fix refresh problem 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -75,7 +75,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
