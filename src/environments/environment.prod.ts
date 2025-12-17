596912da3c05d6c998fb6e191b1ed2b2fc3d42a6
mina <mina.emad.ertxc@gmail.com>
2025-12-17 15:17:36 +0300

    deploy 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,10 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://minaemad-ertxcs.github.io/khedma',
+  apiUrl: 'https://jvsxspnm-3000.uks1.devtunnels.ms',
   baseImagePath: 'C:/Users/hp/Desktop/Mina emad/Repo/images/users/'
 };
+
+
+
+// npx ng b --output-path docs --configuration production --base-href /khedma/
