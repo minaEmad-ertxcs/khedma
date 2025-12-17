4a59e27947afdd95605e787dc741e2ca5aa98aad
mina <mina.emad.ertxc@gmail.com>
2025-12-17 15:49:50 +0300

    add fullName in users

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3001',
   baseImagePath: 'C:/Users/hp/Desktop/Mina emad/Repo/images/users/'
 };
 
